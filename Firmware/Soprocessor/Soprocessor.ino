e16ea23120a7f265fc831252c8d775fcc28f4ac8
V-Nezlo <vlladimirka@gmail.com>
2021-03-12 10:44:22 +0300

    новая схема индикации, правки

--- Firmware/Soprocessor/Soprocessor.ino
+++ Firmware/Soprocessor/Soprocessor.ino
@@ -2,6 +2,7 @@
 В данном случае-в данном случае плата сопроцессора работает как i2c master и запрашивает данные у своего раба
 */
 
+//#include <Arduino.h>
 #include <Wire.h>
 #include <GyverTimer.h>     
 
@@ -14,17 +15,40 @@ char send_value;//что именно отправляем
 int tempin, tempout, humi, voltage;
 char connection;
 char wififlag, workflag, errorflag;
+char wifi_con_status;
 
 GTimer data_request(MS,10000);//Частота запросов для данных
 GTimer data_send(MS,20000); //Частота отправления данных
-GTimer led_status_update(MS,1000); //Частота отправления данных
+GTimer led_status_update(MS,1000); //Частота для светодиодов
+GTimer wifi_con_update(MS,120000);
 
 void update_led(void){
     if (led_status_update.isReady()){
-        wififlag = 0;
         errorflag = 0;
         if (workflag==0) workflag = 1;
         else workflag = 0;
+    }
+}
+
+void check_connection(void){
+    delay(200);
+    Serial.print("AT+CWJAP?");
+    if (Serial.find("No AP")){
+        wifi_con_status = 0;
+        wififlag = 0;
+    }
+    else{
+        wifi_con_status = 1;
+        wififlag = 1;
+    }
+
+}
+
+void check_connection_starter(void){
+    if (wifi_con_update.isReady()){
+        data_send.stop();
+        check_connection();
+        data_send.resume();
     }
 }
 
@@ -35,10 +59,10 @@ void set_indicators(){
 }
 
 void wait_char(char answer[]){
-int safe_counter;
-while(!(Serial.find(answer))){
-	safe_counter++;
-	if (safe_counter>2){
+    int safe_counter;
+    while(!(Serial.find(answer))){
+	    safe_counter++;
+	    if (safe_counter>2){
 		errorflag = 1;
 		return;
 		}
@@ -47,15 +71,15 @@ delay(50);
 }
 
 void wait_char(char answer[],int time){
-int safe_counter;
-while(!(Serial.find(answer))){
+    int safe_counter;
+    while(!(Serial.find(answer))){
 	safe_counter++;
 	if (safe_counter>time){
 		errorflag = 1;
 		return;
 		}
 	}
-delay(50); 
+    delay(50); 
 }
 
 void std_connect(String ssid, String pass){
@@ -63,57 +87,57 @@ void std_connect(String ssid, String pass){
   wait_char("OK");
   Serial.println("AT+CWMODE=1");
   wait_char("OK");
-  Serial.println("AT+CWJAP=\"" + ssid + "\",\"" + pass + "\"");
+  Serial.println("AT+CWJAP_CUR=\"" + ssid + "\",\"" + pass + "\"");
   wait_char("WIFI CONNECTED", 10);
   wait_char("WIFI GOT IP", 10);
   wait_char("OK", 10);
-  wififlag = 1;
   delay(200);
+  check_connection();
 }
 
 void send(String api_w, int field, String data){
-String get = "GET /update?api_key=" + api_w + "&" + "field" + field + "=" + data;
-Serial.println("AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",80");
-wait_char("OK");
-Serial.print("AT+CIPSEND=");
-Serial.println(get.length()+2); 
-delay(300);
-Serial.println(get);
-wait_char("SEND OK");
+    String get = "GET /update?api_key=" + api_w + "&" + "field" + field + "=" + data;
+    Serial.println("AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",80");
+    wait_char("OK");
+    Serial.print("AT+CIPSEND=");
+    Serial.println(get.length()+2); 
+    delay(300);
+    Serial.println(get);
+    wait_char("SEND OK");
 }
 
 void setup(){
-Wire.begin();
-Serial.begin(115200);
-delay(500);
-std_connect("Wi-Fi ZONE", "Mi246589");
-pinMode(led_wifi, OUTPUT);
-pinMode(led_work, OUTPUT);
-pinMode(led_error, OUTPUT);
-pinMode(wifi_wps_pin, INPUT);
+    Wire.begin();
+    Serial.begin(115200);
+    delay(500);
+    //std_connect("Wi-Fi ZONE", "Mi246589");
+    std_connect("Nokia 7.1", "12345678");
+    pinMode(led_wifi, OUTPUT);
+    pinMode(led_work, OUTPUT);
+    pinMode(led_error, OUTPUT);
+    pinMode(wifi_wps_pin, INPUT);
 }
 
 void loop(){
 
     set_indicators();
     update_led();
-if (data_request.isReady()) Wire.requestFrom(17, 9);
-if(Wire.available()) {
-	byte tempinL = Wire.read();
-    byte tempinH = Wire.read();
-    byte humiL = Wire.read();
-    byte humiH = Wire.read();
-    byte tempoutL = Wire.read();
-    byte tempoutH = Wire.read();
-    byte voltageL = Wire.read();
-    byte voltageH = Wire.read();
-    connection = Wire.read();
+    if (data_request.isReady()) Wire.requestFrom(17, 9);
+    if(Wire.available()) {
+	    byte tempinL = Wire.read();
+        byte tempinH = Wire.read();
+        byte humiL = Wire.read();
+        byte humiH = Wire.read();
+        byte tempoutL = Wire.read();
+        byte tempoutH = Wire.read();
+        byte voltageL = Wire.read();
+        byte voltageH = Wire.read();
+        connection = Wire.read();
 
-
-    tempin = tempinL|(tempinH<<8);
-    humi = humiL|(humiH<<8);
-    tempout = tempoutL|(tempoutH<<8);
-    voltage = voltageL|(voltageH<<8);
+        tempin = tempinL|(tempinH<<8);
+        humi = humiL|(humiH<<8);
+        tempout = tempoutL|(tempoutH<<8);
+        voltage = voltageL|(voltageH<<8);
 }
 
 if (data_send.isReady()){
